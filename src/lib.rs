3aa6100c1e60cf5f32a9b2e5ae7bd0034fe08278
Priyadarshan Giri <giripriyadarshan@gmail.com>
2020-07-17 17:42:52 +0530

    added unsigned integers

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,38 @@ pub fn int128conv() -> i128 {
     let s: i128 = s.to_string().trim().parse().unwrap();
     return s;
 }
+pub fn int08conv() -> i8 {
+    let mut s: String = String::new();
+    loop {
+        s.clear();
+        io::stdin().read_line(&mut s).unwrap();
+        if let Ok(s) = s.trim().parse::<i8>() {
+            if s > 0 || s == 0 || s < 0 {
+                break;
+            }
+        } else {
+            println!("please enter numbers only, special characters not supported");
+        }
+    }
+    let s: i8 = s.to_string().trim().parse().unwrap();
+    return s;
+}
+pub fn int16conv() -> i16 {
+    let mut s: String = String::new();
+    loop {
+        s.clear();
+        io::stdin().read_line(&mut s).unwrap();
+        if let Ok(s) = s.trim().parse::<i16>() {
+            if s > 0 || s == 0 || s < 0 {
+                break;
+            }
+        } else {
+            println!("please enter numbers only, special characters not supported");
+        }
+    }
+    let s: i16 = s.to_string().trim().parse().unwrap();
+    return s;
+}
 pub fn int32conv() -> i32 {
     let mut s: String = String::new();
     loop {
@@ -98,5 +130,85 @@ pub fn boolconv() -> bool {
         }
     }
     let s: bool = s.to_string().trim().parse().unwrap();
+    return s;
+}
+pub fn intu128conv() -> u128 {
+    let mut s: String = String::new();
+    loop {
+        s.clear();
+        io::stdin().read_line(&mut s).unwrap();
+        if let Ok(s) = s.trim().parse::<u128>() {
+            if s > 0 || s == 0 {
+                break;
+            }
+        } else {
+            println!("please enter positive u128 numbers only, special characters not supported");
+        }
+    }
+    let s: u128 = s.to_string().trim().parse().unwrap();
+    return s;
+}
+pub fn intu64conv() -> u64 {
+    let mut s: String = String::new();
+    loop {
+        s.clear();
+        io::stdin().read_line(&mut s).unwrap();
+        if let Ok(s) = s.trim().parse::<u64>() {
+            if s > 0 || s == 0 {
+                break;
+            }
+        } else {
+            println!("please enter positive u64 numbers only, special characters not supported");
+        }
+    }
+    let s: u64 = s.to_string().trim().parse().unwrap();
+    return s;
+}
+pub fn intu32conv() -> u32 {
+    let mut s: String = String::new();
+    loop {
+        s.clear();
+        io::stdin().read_line(&mut s).unwrap();
+        if let Ok(s) = s.trim().parse::<u32>() {
+            if s > 0 || s == 0 {
+                break;
+            }
+        } else {
+            println!("please enter positive u32 numbers only, special characters not supported");
+        }
+    }
+    let s: u32 = s.to_string().trim().parse().unwrap();
+    return s;
+}
+pub fn intu16conv() -> u16 {
+    let mut s: String = String::new();
+    loop {
+        s.clear();
+        io::stdin().read_line(&mut s).unwrap();
+        if let Ok(s) = s.trim().parse::<u16>() {
+            if s > 0 || s == 0 {
+                break;
+            }
+        } else {
+            println!("please enter positive u16 numbers only, special characters not supported");
+        }
+    }
+    let s: u16 = s.to_string().trim().parse().unwrap();
+    return s;
+}
+pub fn intu8conv() -> u8 {
+    let mut s: String = String::new();
+    loop {
+        s.clear();
+        io::stdin().read_line(&mut s).unwrap();
+        if let Ok(s) = s.trim().parse::<u8>() {
+            if s > 0 || s == 0 {
+                break;
+            }
+        } else {
+            println!("please enter positive u16 numbers only, special characters not supported");
+        }
+    }
+    let s: u8 = s.to_string().trim().parse().unwrap();
     return s;
 }
